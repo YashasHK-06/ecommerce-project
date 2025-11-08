a07612f1b57efa3633c2d3145a07d919cf9b5e21
YashasHK-06 <yashas.hk06@gmail.com>
2025-11-08 19:23:17 +0530

    Fix: Reorder imports in index.js

--- mernProjectEcommerce-master/frontend/src/index.js
+++ mernProjectEcommerce-master/frontend/src/index.js
@@ -1,13 +1,15 @@
-import axios from "axios";
-axios.defaults.baseURL = "https://yashas-ecommerce-api.onrender.com";
+// ALL IMPORTS MUST COME FIRST
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import axios from "axios"; // This import was moved up with the others
+
+// ALL OTHER CODE (like setting baseURL) comes AFTER imports
+axios.defaults.baseURL = "https://yashas-ecommerce-api.onrender.com";
 
 const options = {
   timeout: 5000,
